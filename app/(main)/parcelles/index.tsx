5e3b601d7ca3566c436327b7fb42c53222af65ad
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-03-03 22:23:33 +0000

    feat: Création des écrans vides (Home, Parcelles, Récoltes, Profil).

---
+++ app/(main)/parcelles/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function ParcellesScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Mes Parcelles</Text>
+    </View>
+  );
+}
---
+++ app/(main)/recolte/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function RecolteScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Mes Récoltes</Text>
+    </View>
+  );
+}
