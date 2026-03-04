a2deba71793e39c8a1253203ab87016d24978b0c
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-03-04 11:51:04 +0000

    feat: Ajouter le bouton de déconnexion dans le Drawer.

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -1,10 +1,40 @@
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { router } from "expo-router";
 import { Drawer } from "expo-router/drawer";
+import { getAuth, signOut } from "firebase/auth";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { COLORS } from "../../constants/colors";
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
+
 
 export default function Layout() {
+  const handleLogout = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        return router.replace("/auth/login");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer>
+      <Drawer
+        drawerContent={(props) => (
+          <DrawerContentScrollView {...props}>
+            <DrawerItemList {...props} />
+            <View style={logoutStyle.footer}>
+              <TouchableOpacity onPress={handleLogout} style={logoutStyle.btn}>
+                <Text style={logoutStyle.btnText}>Déconnexion</Text>
+              </TouchableOpacity>
+            </View>
+          </DrawerContentScrollView>
+        )}
+      >
         <Drawer.Screen
           name="index"
           options={{
@@ -37,3 +67,20 @@ export default function Layout() {
     </GestureHandlerRootView>
   );
 }
+
+const logoutStyle = StyleSheet.create({
+  footer: {
+    padding: 16,
+    borderTopWidth: 1,
+    borderTopColor: COLORS.border.divider,
+  },
+  btn: {
+    backgroundColor: COLORS.background.error,
+    borderRadius: 20,
+    padding: 12,
+    alignItems: "center",
+  },
+  btnText: {
+    color: COLORS.text.error,
+  },
+});
