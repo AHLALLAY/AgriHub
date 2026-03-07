438cd29ced7959936e8e745f1fc99014f83d292c
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-03-07 16:24:44 +0000

    feat: Ajouter la méthode 'getParcellesByUser'

--- services/parcellesService.ts
+++ services/parcellesService.ts
@@ -1,6 +1,6 @@
 import { db } from "@/config/firebaseConfig";
-import { ParcelleInput } from "@/types/parecelle";
-import { addDoc, collection } from "firebase/firestore";
+import { Parcelle, ParcelleInput } from "@/types/parecelle";
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
 export async function createParcelle(
   userId: string,
   data: Omit<ParcelleInput, "userId">,
@@ -11,4 +11,16 @@ export async function createParcelle(
     createdAt: new Date().toISOString(),
   });
   return ref.id;
+}
+
+export async function getParcellesByUser(userId: string): Promise<Parcelle[]> {
+  const myQery = query(
+    collection(db, "parcelles"),
+    where("userId", "==", userId),
+  );
+  const data = await getDocs(myQery);
+  return data.docs.map((d) => ({
+    id: d.id,
+    ...d.data(),
+  })) as Parcelle[];
 }
